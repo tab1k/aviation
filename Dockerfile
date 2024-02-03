b3b3dc197315210201f0fe10315b50ef6ad0cbd1
Карбаев Табигат <tabigat@MacBook-Pro-Karbaev.local>
2024-02-03 20:35:11 +0600

    adding nginx

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3
+FROM python:3.10
 
 # Set env variables
 RUN apt-get update
--- aviation/settings.py
+++ aviation/settings.py
@@ -94,18 +94,18 @@ WSGI_APPLICATION = "aviation.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            'NAME': os.getenv('DB_NAME', 'tabigat_aviation'),
-            'USER': os.getenv('DB_USER', 'tabigat_user'),
-            'PASSWORD': os.getenv('DB_PASSWORD', 'tabigat_password'),
-            'HOST': os.getenv('DB_HOST', 'db'),
-            'PORT': os.getenv('DB_PORT', 5432),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+    # "default": {
+    #         "ENGINE": "django.db.backends.postgresql",
+    #         'NAME': os.getenv('DB_NAME', 'tabigat_aviation'),
+    #         'USER': os.getenv('DB_USER', 'tabigat_user'),
+    #         'PASSWORD': os.getenv('DB_PASSWORD', 'tabigat_password'),
+    #         'HOST': os.getenv('DB_HOST', 'db'),
+    #         'PORT': os.getenv('DB_PORT', 5432),
+    # }
 }
 
 
