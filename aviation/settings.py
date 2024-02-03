877d9451ce7d38c77f84411c9aa70a5b093fac23
Карбаев Табигат <tabigat@MacBook-Pro-Karbaev.local>
2024-02-03 20:36:17 +0600

    adding nginx

--- aviation/settings.py
+++ aviation/settings.py
@@ -145,6 +145,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'users', 'static'),
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://stransit.kz']
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
